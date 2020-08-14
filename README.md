06328beb06642e480fb83cc4e6f3cea608e8e193
Vorono4ka <54549682+Vorono4ka@users.noreply.github.com>
2020-08-14 22:47:59 +0400

    Revert "Initial commit"
    This reverts commit e69521b63dc85bfc0b77cdb3c20ed8887e9592a4.

--- README.md
+++
@@ -1,8 +0,0 @@
-### SC Compression
-
-Version 0.2.4
--
-
-### Tools:
-- Decompression
-- Compression
--- build.bat
+++
@@ -1,8 +0,0 @@
-@ECHO off
-SET FOLDER="dist"
-DEL /F/Q/S "%FOLDER%" > NUL
-RMDIR /Q/S "%FOLDER%"
-
-python setup.py sdist bdist_wheel
-python -m twine upload --repository testpypi dist/*
-python -m twine upload --repository pypi dist/*
